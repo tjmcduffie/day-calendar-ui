3a028277c935192343c7d102f778a1538697fe27
tim mcduffie <timothy.mcduffie@gmail.com>
2014-12-08 11:30:38 -0500

    added directory page, reports and updated .gitignore

---
+++ reports/docs/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Calendar",
+        "CalendarEvent",
+        "DayView",
+        "EventEmitter",
+        "Logger"
+    ],
+    "modules": [
+        "DayCalendar"
+    ],
+    "allModules": [
+        {
+            "displayName": "DayCalendar",
+            "name": "DayCalendar",
+            "description": "DayCalendar creates the UI behaviors and logic for managing the day view of the Calendar."
+        }
+    ]
+} };
+});
