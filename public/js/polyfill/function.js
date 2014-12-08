5dd859e84c0666e2673cd9d7638cd42d3ca62e2b
tim mcduffie <timothy.mcduffie@gmail.com>
2014-12-07 21:00:00 -0500

    new polyfill for function.prototype.bind

---
+++ public/js/polyfill/function.js
@@ -0,0 +1,30 @@
+
+/**
+ * Polyfill for Function.prototype.bind. This ES5 method is unsupported in IE8 and below, PhantomJS and
+ * legacy versions of other browsers.
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Polyfill
+ */
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP && oThis
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
